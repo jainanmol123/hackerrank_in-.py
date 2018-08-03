ded3576f74768d10dddec77fb74ec7b4e94dbb2d
Anmol jain <jainanmol810@gmail.com>
2018-08-03 18:31:35 +0530

    validating UID

---
+++ validating_UID.py
@@ -0,0 +1,9 @@
+import re 
+for i in range(int(input())):
+    s=''.join(sorted(input()))
+    if re.search(r'^[a-zA-Z0-9]{10}$',s)and re.search(r'[A-Z]{2,}',s)and re.search(r'\d{3,}',s) and not re.search(r'(.)\1',s):
+        print("Valid")
+    else:
+        print("Invalid")
+               
+        
