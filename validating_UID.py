3ccb2d4c270044315ee5e55eb73d46bd40499b28
Shan-56 <56685912+Shan-56@users.noreply.github.com>
2019-10-17 19:15:05 +0530

    Update validating_UID.py

--- validating_UID.py
+++ validating_UID.py
@@ -4,6 +4,6 @@ for i in range(int(input())):
     if re.search(r'^[a-zA-Z0-9]{10}$',s)and re.search(r'[A-Z]{2,}',s)and re.search(r'\d{3,}',s) and not re.search(r'(.)\1',s):
         print("Valid")
     else:
-        print("Invalid")
+        print("Invalid"
                
         
